d9340491d3e1dc388c8b56a37deed935cb9c8720
Anshika1-23 <126404699+Anshika1-23@users.noreply.github.com>
2024-05-02 21:52:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,1 @@
-const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://localhost:27017/mentorship_network'; // Replace with your MongoDB URI
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
-
-
-  const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-  });
-  
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports = User;
