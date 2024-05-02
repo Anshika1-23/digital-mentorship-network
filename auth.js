559a961025901ec706394385d694d1958f23557e
Anshika1-23 <126404699+Anshika1-23@users.noreply.github.com>
2024-05-02 21:34:33 +0530

    delete this file

--- auth.js
+++ auth.js
@@ -9,29 +9,4 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, don
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        return done(null, false, { message: 'Incorrect email.' });
-      }
-      bcrypt.compare(password, user.password, (err, isMatch) => {
-        if (err) throw err;
-        if (isMatch) {
-          return done(null, user);
-        } else {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
-      });
-    })
-    .catch(err => console.log(err));
-}));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
-};
-
-module.exports = initializePassport;
+        return done(null, false, 
