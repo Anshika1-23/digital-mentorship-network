3b95090ba1d6c8fd3e7e7e8cde7513b531a305a4
Anshika1-23 <126404699+Anshika1-23@users.noreply.github.com>
2024-05-02 21:36:40 +0530

    delete auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,1 @@
-// auth.js
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-const User = require('./models/User');
 
-const initializePassport = (passport) => {
-passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-  User.findOne({ email: email })
-    .then(user => {
-      if (!user) {
-        return done(null, false, 
