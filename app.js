65da98404d23386a296b8d4774c48bfffa32424e
Anshika1-23 <126404699+Anshika1-23@users.noreply.github.com>
2024-05-02 21:43:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const mentorRoutes = require('./routes/mentorRoutes');
 const menteeRoutes = require('./routes/menteeRoutes');
 const registerRoutes = require('./routes/registerRoutes');
 const loginRoutes = require('./routes/loginRoutes');
+const logoutRoutes = require('./routes/logoutRoutes');
 const config = require('./config/config');
 const path = require('path');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -22,11 +24,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.use('/mentors', mentorRoutes);
 app.use('/mentees', menteeRoutes);
 app.use('/register', registerRoutes);
 app.use('/login', loginRoutes);
+app.use('/logout', logoutRoutes);
 
+app.use('/mentors', auth, mentorRoutes);
+app.use('/mentees', auth, menteeRoutes);
 
 // Route for serving the register.html file
 app.get('/register', (req, res) => {
@@ -36,6 +40,15 @@ app.get('/register', (req, res) => {
 // Route for serving the login.html file
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/mentors', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'mentors.html'));
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile = path.join(__dirname, 'public', 'index.html');
+
 });
 
 // Error handling middleware
